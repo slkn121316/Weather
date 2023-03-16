57c03818d320e38f413b59d6c95a146e5789a396
slkn121316 <68160810+slkn121316@users.noreply.github.com>
2023-03-17 02:19:44 +0530

    vercel deployment settings

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo " BUILD START "
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py colectstatic --noinput --clear
+python -m pip install -r requirements.txt
+python manage.py colectstatic --noinput --clear
 echo " BUILD END "
